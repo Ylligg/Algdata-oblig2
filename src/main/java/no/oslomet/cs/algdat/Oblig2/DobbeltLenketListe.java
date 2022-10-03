e709e27fec9268d1adf372c31e13091c5cfc901e
Ylligg <yll.gashi@hotmail.com>
2022-10-03 23:47:04 +0200

    fiksa oppgave 1 på en riktig måte med hode og hale

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -44,11 +44,9 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
 
     public DobbeltLenketListe(T[] a) { // skal lage listen
-    this.hode = null;
-    this.hale = null;
 
 
-        if(a == null) { // riktig
+        if(a == null) {
             Objects.requireNonNull(a, "Tabellen a er null!"); // gir en melding hvis tabellen er null
         }
 
@@ -56,34 +54,22 @@ public class DobbeltLenketListe<T> implements Liste<T> {
             int i =0;
             for(; i < a.length;i++) {
 
-                if (a[i] != null) { // om verdien er ikke null så addes den inn i listen
-                    hode = new Node<>(a[i]); // lager en ny node
+                if (a[i] != null) { // om verdien er ikke null så lages det en ny node
+                    hode = new Node<>(a[i]); // den nye noden blir hoden siden den blir adda fra ventre siden
                     antall++; // øker variabel antall av antall elementer i tabellen
-                    break;
+                    break; // går ut av løkka
                 }
             }
-            hale = hode;
+            hale = hode; // hvis det er bare en verdi så vil hode og hale være i samme posisjon
             i++;
             for(; i < a.length;i++){
                 if (a[i] != null) {
-
+                    hale = new Node<>(a[i]); // nye noden blir hale
+                    hale.neste = hale; // halen fortsetter til slutten av tabellen
+                    antall++;
+                }
             }
         }
-
-
-
-        // må finne en måte å implimintere koden for linked lists
-
-
-            antall++;
-
-            if (a[i] == null) { // finne posisjonen til node tabellen, om den er null så skal den ikke tas med
-               antall--;// om det er en posisjon som er null så reduseres antallet med en
-            }
-
-
-        }
-        //System.out.println(liste + " hei");
     }
 
 
@@ -103,7 +89,7 @@ public class DobbeltLenketListe<T> implements Liste<T> {
     }
 
     @Override
-    public boolean leggInn(T verdi) { // finn en måte å legge inn verdier på idk how.
+    public boolean leggInn(T verdi) {
 
         // må finne en måte å imlimintere hode slik at den kan oppdateres til å peke mot den nye Noden siden når vi adder ny Node så
 
@@ -111,7 +97,6 @@ public class DobbeltLenketListe<T> implements Liste<T> {
             Objects.requireNonNull(verdi, "Verdien er null!"); // gir en melding hvis tabellen er null
             return false;
         } else{
-            liste.add(verdi);
             antall++;
             return true;
         }
@@ -161,7 +146,7 @@ public class DobbeltLenketListe<T> implements Liste<T> {
     @Override
     public String toString() { // oppgave 2 hode->hale (finn en måte å adde inn veridene)
         StringJoiner joiner = new StringJoiner(",");
-        joiner.add(liste.toString());
+
 
         return joiner.toString();
     }
@@ -170,13 +155,7 @@ public class DobbeltLenketListe<T> implements Liste<T> {
     public String omvendtString() { // oppgave 2 hale->hode
         StringJoiner joiner = new StringJoiner(","); // trenger denne
 
-        // dette er bare feil
 
-        for(int i = liste.size(); i>0;i--){
-            omvendtliste.add(i);
-        }
-        joiner.add(omvendtliste.toString());
-        omvendtliste.clear(); // skal egt ikke ha dette men må pga løkka
 
         return joiner.toString(); // riktig
 
